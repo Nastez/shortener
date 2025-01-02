ce927ae1ded263df19310039f588d4071d511b93
Anastasiya Mineeva <asmineeva@avito.ru>
2025-01-02 21:06:53 +0300

    iter4: added new config

--- config/config.go
+++ config/config.go
@@ -4,11 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
-	"github.com/Nastez/shortener/utils"
 )
 
-var invalidPort = utils.GenerateID()
+var invalidPort string
 
 var FlagRunAddr string
 var FlagBaseAddr string
@@ -16,6 +14,7 @@ var FlagBaseAddr string
 // ParseFlags обрабатывает аргументы командной строки
 // и сохраняет их значения в соответствующих переменных
 func ParseFlags() {
+
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
@@ -23,8 +22,13 @@ func ParseFlags() {
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 
-	if FlagBaseAddr == "http://localhost:" || FlagBaseAddr == "http://localhost:/" || FlagBaseAddr == "http://localhost:"+invalidPort || FlagBaseAddr == "http://localhost:/"+invalidPort {
+	if FlagBaseAddr == "http://localhost:" || FlagBaseAddr == "http://localhost:/" {
 		fmt.Fprintf(os.Stderr, "Invalid base address: %s (must have format http://localhost:8080/)\n", FlagBaseAddr)
+		os.Exit(1)
+	}
+
+	if len([]rune(FlagRunAddr)) > 5 {
+		fmt.Fprintf(os.Stderr, "Invalid port: %s (port must have format :8080)\n", FlagBaseAddr)
 		os.Exit(1)
 	}
 }
